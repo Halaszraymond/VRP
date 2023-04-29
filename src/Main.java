26175853c43803e191c344877f41821e4d8f3910
Raymond Visser <raymond@halasz.nl>
2023-04-29 23:02:59 +0200

    Added a few extra postal codes

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ public class Main {
     private static String geocoderAPIKey = "c9941b30ff46475186fc03e68861bf3d";
     // apikey to openroute service (Create TimeDistanceMatrix)
     private static String openRouteServiceAPIKey = "5b3ce3597851110001cf62486ddae1168c244a1fb63e38ce8c161e4a";
-    // List of postal codes, we need to extract these from the database
-    private static String[] postalCodes = new String[]{"6712CP", "5051RB", "1106NG", "7141LD", "7881JM", "1132PA", "5035BM", "7203EA", "6171VS", "4641SJ", "5991NJ", "3032AG", "1991AT", "7523XZ", "7232DS", "5037HL"};
+    // List of postal codes, first PostalCode is the DEPOT!!! we need to extract these from the database (Use https://www.bestrandoms.com/random-address-in-nl for random but EXISTING Dutch postal codes)
+    private static String[] postalCodes = new String[]{"6712CP", "5051RB", "1106NG", "7141LD", "7881JM", "1132PA", "5035BM", "7203EA", "6171VS", "4641SJ", "5991NJ", "3032AG", "1991AT", "7523XZ", "7232DS", "5037HL", "5731XX", "8251KW", "3341LB", "6591TW", "6021EG"};
     // Number of vehicles
     private static int numberOfVehicles = 4;
     // Location of the depot
