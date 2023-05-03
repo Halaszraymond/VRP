8e8251597cb9b6cbe3728debd5c648a3f2b3c37e
Raymond Visser <raymond@halasz.nl>
2023-05-03 16:16:41 +0200

    initialized a long[] list, not that important

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
     // List of postal codes, first PostalCode is the DEPOT!!! we need to extract these from the database (Use https://www.bestrandoms.com/random-address-in-nl for random but EXISTING Dutch postal codes)
     private static String[] postalCodes = new String[]{"6712CP", "5051RB", "1106NG", "7141LD", "7881JM", "1132PA", "5035BM", "7203EA", "6171VS", "4641SJ", "5991NJ", "3032AG", "1991AT", "7523XZ", "7232DS", "5037HL", "5731XX", "8251KW", "3341LB", "6591TW", "6021EG"};
     // Demands per postal Code, keep in mind that the total demands must not exceed the total capacity of all vehicles
-    private static long[] demands = {0, 1, 1, 2, 4, 2, 4, 8, 8, 1, 2, 1, 2, 4, 4, 8, 8, 6, 4, 5, 7};
+    private static long[] demands = new long[]{0, 1, 1, 2, 4, 2, 4, 8, 8, 1, 2, 1, 2, 4, 4, 8, 8, 6, 4, 5, 7};
     // Capacity of each vehicle, keep in mind that the total demands must not exceed the total capacity of all vehicles
     private static long vehicleCapacity = 20;
     // Number of vehicles
